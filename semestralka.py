05da03ccc341bb38030d620da33232c003abb643
Susssiee <32455031+Susssiee@users.noreply.github.com>
2018-05-08 18:39:36 +0200

    Add files via upload
    Added functions canvasEvent, sizeEvent, loadImage, showImage and binded empty image operation functions

--- semestralka.py
+++ semestralka.py
@@ -5,6 +5,65 @@ import numpy as np
 
 
 class ImageEditor:
+	imageLoaded = 0
+	width = 408
+	height = 347
+	picture = ""
+
+	def show_image(self):
+		self.img = ImageTk.PhotoImage(self.picture)
+		self.canvas.create_image(self.width/2,self.height/2, anchor = CENTER, image = self.img)
+
+	def negative (self):
+		return
+
+	def rotateLeft (self):
+		return
+
+	def rotateRight (self):
+		return
+
+	def flipX (self):
+		return
+
+	def flipY (self):
+		return
+
+	def makeGray (self):
+		return
+
+	def brightenUp (self):
+		return
+
+	def brightenDown (self):
+		return
+
+	def contrastUp (self):
+		return
+
+	def contrastDown (self):
+		return
+
+	def loadImage(self):
+		fname = filedialog.askopenfilename()
+		if fname:
+			try:
+				self.picture = Image.open(fname)
+				self.show_image()
+				self.imageLoaded =1
+			except:
+				self.status['text'] = "Unsupported file format"
+
+	def sizeEvent (self, event):
+		self.status['text'] = str(event.width) +"x"+str(event.height)
+
+	def canvasEvent(self, event):
+		self.width = event.width
+		self.height = event.height
+		if self.imageLoaded:
+			self.canvas.delete("all")
+			self.show_image()
+
 	def __init__(self,master):
 		master.title("Image Editor")
 		rootFrame 	= Frame(master, bg = "#404040")
@@ -15,10 +74,11 @@ class ImageEditor:
 		imageFrame.pack(fill ="both", expand = True, side = LEFT)
 		buttonFrame.pack(fill ="y", side = RIGHT, anchor =E)
 		statusFrame.pack(fill ="x", side = BOTTOM, anchor =S)
+		rootFrame.bind("<Configure>", self.sizeEvent)
 
 		menubar =Menu(root, background = "#bbbbc9", activebackground = "#adadba",relief = FLAT)
 		filemenu = Menu(menubar, tearoff=0, background = "#bbbbc9", activebackground = "#adadba",relief = FLAT)
-		filemenu.add_command(label="Load image", command=self.say_hi, background = "#bbbbc9", activebackground = "#adadba")
+		filemenu.add_command(label="Load image", command=self.loadImage, background = "#bbbbc9", activebackground = "#adadba")
 		filemenu.add_command(label="Save image", command=self.say_hi, background = "#bbbbc9", activebackground = "#adadba")
 		filemenu.add_separator()
 		filemenu.add_command(label="Quit", command=master.quit, background = "#bbbbc9", activebackground = "#adadba")
@@ -30,19 +90,20 @@ class ImageEditor:
 
 		self.canvas = Canvas(imageFrame, width = 300, height = 300, bg = "#404040", highlightthickness= 0, relief = FLAT)
 		self.canvas.pack(fill ="both", expand = True)
+		self.canvas.bind("<Configure>", self.canvasEvent)
 
 
 		#Buttons
-		self.rotateL   = Button(buttonFrame, text="rotateL", command = self.say_hi, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
-		self.rotateR   = Button(buttonFrame, text="rotateR", command = self.say_hi, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
-		self.mirrorX   = Button(buttonFrame, text="mirrorX", command = self.say_hi, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
-		self.mirrorY   = Button(buttonFrame, text="mirrorY", command = self.say_hi, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
-		self.inverse   = Button(buttonFrame, text="inverse", command = self.say_hi, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
-		self.gray 	   = Button(buttonFrame, text="toGray", command = self.say_hi, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
-		self.bright    = Button(buttonFrame, text="brigthen", command = self.say_hi, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
-		self.dark 	   = Button(buttonFrame, text="darken", command = self.say_hi, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
-		self.contrastM = Button(buttonFrame, text="moreContrast", command = self.say_hi, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
-		self.contrastL = Button(buttonFrame, text="lessContrast", command = self.say_hi, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
+		self.rotateL   = Button(buttonFrame, text="rotateL", command = self.rotateLeft, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
+		self.rotateR   = Button(buttonFrame, text="rotateR", command = self.rotateRight, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
+		self.mirrorX   = Button(buttonFrame, text="mirrorX", command = self.flipX, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
+		self.mirrorY   = Button(buttonFrame, text="mirrorY", command = self.flipY, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
+		self.inverse   = Button(buttonFrame, text="inverse", command = self.negative, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
+		self.gray 	   = Button(buttonFrame, text="toGray", command = self.makeGray, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
+		self.bright    = Button(buttonFrame, text="brigthen", command = self.brightenUp, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
+		self.dark 	   = Button(buttonFrame, text="darken", command = self.brightenDown, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
+		self.contrastM = Button(buttonFrame, text="moreContrast", command = self.contrastUp, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
+		self.contrastL = Button(buttonFrame, text="lessContrast", command = self.contrastDown, bg = "#404040", fg = "#fff3d3", activebackground = "#8fa876", relief = FLAT, highlightthickness = 0, cursor = "hand2").pack(fill = "x")
 
 	def say_hi(self):
 		print("hello")
